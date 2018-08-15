bd5f1a3ed1ae9d270e0854c156ce06d19019f6ba
Maximus090 <42406184+Maximus090@users.noreply.github.com>
2018-08-15 13:36:18 +0300

    Add files via upload

---
+++ 1/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+function prost($x)
+{
+    if ($x==2)
+        return true;
+    if ($x%2==0)
+        return false;
+    for($i=2; $i*$i<$x; $i++) {
+        if($x%$i==0)
+            return false;
+    }
+    return true;
+}
+$sum = 0;
+for ($i=2; $i<2000000; $i++) {
+    if (prost($i))
+        $sum+=$i;
+}
+echo "Сумма простых чисел до 2000000 = ".$sum;
+?>
